0b54b307b9eaae1b72210ef7bf495ddfb7d8dd13
Rudi Blaha Svartveit <rudsvar@gmail.com>
2022-10-09 13:04:11 +0200

    Add more layers

--- src/api/rest/mod.rs
+++ src/api/rest/mod.rs
@@ -4,22 +4,43 @@ use axum::{
     middleware::{self, Next},
     response::IntoResponse,
     routing::post,
-    Router, Extension,
+    Extension, Router,
 };
-use hyper::{Body, Request, Response, StatusCode};
+use hyper::{
+    header::{HeaderName, AUTHORIZATION},
+    Body, Request, Response, StatusCode,
+};
 use sqlx::PgPool;
-use std::net::TcpListener;
+use std::{iter::once, net::TcpListener};
+use tower_http::{
+    propagate_header::PropagateHeaderLayer,
+    request_id::{MakeRequestUuid, SetRequestIdLayer},
+    sensitive_headers::SetSensitiveRequestHeadersLayer,
+    trace::{DefaultOnRequest, DefaultOnResponse, TraceLayer},
+};
+use tracing::Level;
 
 pub mod hello;
 pub mod item_api;
 
+static X_REQUEST_ID: &str = "x-request-id";
+
 pub async fn axum_server(addr: TcpListener, db: PgPool) -> Result<(), hyper::Error> {
+    let request_id = HeaderName::from_static(X_REQUEST_ID);
     let app = Router::new()
         .route("/", post(|| async move { "Hello from `POST /`" }))
-        .nest("/", hello::hello_routes())
-        .nest("/", item_api::item_routes())
+        .merge(hello::hello_routes())
+        .merge(item_api::item_routes())
         .layer(Extension(db))
         .layer(middleware::from_fn(print_request_response))
+        .layer(PropagateHeaderLayer::new(request_id.clone()))
+        .layer(
+            TraceLayer::new_for_http()
+                .on_request(DefaultOnRequest::new().level(Level::INFO))
+                .on_response(DefaultOnResponse::new().level(Level::INFO)),
+        )
+        .layer(SetRequestIdLayer::x_request_id(MakeRequestUuid))
+        .layer(SetSensitiveRequestHeadersLayer::new(once(AUTHORIZATION)))
         .into_make_service();
     tracing::info!("Starting Axum on {:?}", addr.local_addr());
     let axum_server = axum::Server::from_tcp(addr)?
--- src/service/greeter.rs
+++ src/service/greeter.rs
@@ -1,4 +1,7 @@
+use tracing::instrument;
+
 /// Greets someone by name.
+#[instrument(ret)]
 pub fn greet(name: &str) -> String {
     format!("Hello, {}!", name)
 }
