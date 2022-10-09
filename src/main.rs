4d39f2fe1e6eaf61889d9462a3006019c5ac856c
Rudi Blaha Svartveit <rudsvar@gmail.com>
2022-10-09 13:09:50 +0200

    Make stdout non blocking

--- src/main.rs
+++ src/main.rs
@@ -17,19 +17,22 @@ async fn main() -> anyhow::Result<()> {
     // Set up logging
     let log_level = std::env::var("RUST_LOG").unwrap_or_else(|_| "info,axum_web_demo=debug".into());
 
-    let stdout = tracing_subscriber::fmt::layer().with_filter(EnvFilter::new(log_level));
+    let (non_blocking_stdout, _guard) = tracing_appender::non_blocking(std::io::stdout());
+    let stdout = tracing_subscriber::fmt::layer()
+        .with_writer(non_blocking_stdout)
+        .with_filter(EnvFilter::new(log_level));
 
     let file_appender = tracing_appender::rolling::hourly("./logs", "log.");
-    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-    let file_writer = tracing_subscriber::fmt::layer()
+    let (non_blocking_file_appender, _guard) = tracing_appender::non_blocking(file_appender);
+    let file_appender = tracing_subscriber::fmt::layer()
         .with_ansi(false)
-        .with_writer(non_blocking)
+        .with_writer(non_blocking_file_appender)
         .json()
         .with_filter(EnvFilter::new("info,axum_web_demo=trace"));
 
     let reg = tracing_subscriber::registry()
         .with(stdout)
-        .with(file_writer);
+        .with(file_appender);
 
     reg.init();
 
