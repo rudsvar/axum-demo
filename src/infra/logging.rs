a993e8c23461f7a2d3d632d0dbfa5cff2f232009
Rudi Blaha Svartveit <rudsvar@gmail.com>
2023-06-26 18:59:54 +0200

    Revert "Remove opentelemetry and jaeger support"
    This reverts commit e0d7df76d5ee0c1ffe6971953b5078b5ba8bfd42.

--- src/infra/logging.rs
+++ src/infra/logging.rs
@@ -17,7 +17,14 @@ pub fn init_logging() -> LogGuard {
     let (non_blocking_stdout, stdout_guard) = tracing_appender::non_blocking(std::io::stdout());
     let stdout = tracing_subscriber::fmt::layer()
         .with_writer(non_blocking_stdout)
-        .with_filter(EnvFilter::new(log_level));
+        .with_filter(EnvFilter::new(log_level.clone()));
+
+    let app_name = env!("CARGO_PKG_NAME");
+    let opentelemetry_tracer = opentelemetry_jaeger::new_agent_pipeline()
+        .with_service_name(app_name)
+        .install_simple()
+        .unwrap();
+    let opentelemetry = tracing_opentelemetry::layer().with_tracer(opentelemetry_tracer).with_filter(EnvFilter::new(log_level));
 
     let console_layer = if cfg!(debug_assertions) {
         Some(console_subscriber::spawn())
@@ -27,6 +34,7 @@ pub fn init_logging() -> LogGuard {
 
     let reg = tracing_subscriber::registry()
         .with(stdout)
+        .with(opentelemetry)
         .with(console_layer)
         .with(ErrorLayer::default());
 
